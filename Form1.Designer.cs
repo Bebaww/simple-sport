a0981232888151b4c7e1e457ba0c7f7df63cc71c
Bebaw <bekabebaw@gmail.com>
2023-04-11 02:03:37 -1200

    simple sport project

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_exercise = new System.Windows.Forms.TextBox();
+            this.txt_reps = new System.Windows.Forms.TextBox();
+            this.txt_sets = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_bmi = new System.Windows.Forms.TextBox();
+            this.txt_height = new System.Windows.Forms.TextBox();
+            this.txt_weight = new System.Windows.Forms.TextBox();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_exercise
+            // 
+            this.txt_exercise.Location = new System.Drawing.Point(211, 175);
+            this.txt_exercise.Name = "txt_exercise";
+            this.txt_exercise.Size = new System.Drawing.Size(84, 20);
+            this.txt_exercise.TabIndex = 0;
+            // 
+            // txt_reps
+            // 
+            this.txt_reps.Location = new System.Drawing.Point(211, 214);
+            this.txt_reps.Name = "txt_reps";
+            this.txt_reps.Size = new System.Drawing.Size(65, 20);
+            this.txt_reps.TabIndex = 1;
+            // 
+            // txt_sets
+            // 
+            this.txt_sets.Location = new System.Drawing.Point(211, 265);
+            this.txt_sets.Name = "txt_sets";
+            this.txt_sets.Size = new System.Drawing.Size(65, 20);
+            this.txt_sets.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "exercise";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "reps";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "sets";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(152, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(147, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "weight";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(149, 101);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "height";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(152, 140);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "bmi";
+            // 
+            // txt_bmi
+            // 
+            this.txt_bmi.Location = new System.Drawing.Point(211, 133);
+            this.txt_bmi.Name = "txt_bmi";
+            this.txt_bmi.Size = new System.Drawing.Size(65, 20);
+            this.txt_bmi.TabIndex = 10;
+            // 
+            // txt_height
+            // 
+            this.txt_height.Location = new System.Drawing.Point(211, 101);
+            this.txt_height.Name = "txt_height";
+            this.txt_height.Size = new System.Drawing.Size(84, 20);
+            this.txt_height.TabIndex = 11;
+            // 
+            // txt_weight
+            // 
+            this.txt_weight.Location = new System.Drawing.Point(210, 62);
+            this.txt_weight.Name = "txt_weight";
+            this.txt_weight.Size = new System.Drawing.Size(85, 20);
+            this.txt_weight.TabIndex = 12;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(210, 29);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(146, 20);
+            this.txt_name.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.txt_weight);
+            this.Controls.Add(this.txt_height);
+            this.Controls.Add(this.txt_bmi);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_sets);
+            this.Controls.Add(this.txt_reps);
+            this.Controls.Add(this.txt_exercise);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_exercise;
+        private System.Windows.Forms.TextBox txt_reps;
+        private System.Windows.Forms.TextBox txt_sets;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_bmi;
+        private System.Windows.Forms.TextBox txt_height;
+        private System.Windows.Forms.TextBox txt_weight;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            double h = Convert.ToDouble(txt_height.Text);
+           double W=Convert.ToDouble(  txt_weight.Text);
+           double bmi = Convert.ToDouble(W / (h * h));
+            txt_bmi.Text = bmi.ToString();
+
+            MessageBox.Show("name:" + txt_name.Text + "\n" +
+                "weight:" + txt_weight.Text + "kg" + "\n"+
+                "height:" + txt_height.Text + "m"+ "\n"+
+                "bmi:" + txt_bmi.Text +"\n"+
+                "exercise:" + txt_exercise.Text +"\n"+
+                "reps:" + txt_reps.Text);
+
+
+        }
+    }
+}
